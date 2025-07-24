9732580a7dbf99d72aeeeb6fe98b742300b39081
hwk3005 <hwk3005@naver.com>
2025-07-24 21:17:35 +0900

    250724_3_자습

---
+++ Ja0724/src/C0724_16_자습.java
@@ -0,0 +1,23 @@
+
+public class C0724_16_자습 {
+	public static void main(String[] args) {
+		
+//		p132 등가비교연산자
+		System.out.println('0' == 48);
+		System.out.println('A' == 65);
+		
+		
+//		p152 삼항식 -> if문
+		int x = 1;
+		int y = 2;
+		int result = 0;
+		
+		if (x>y){
+			result = x;
+		}else {
+			result = y;
+		}
+		System.out.println(result);
+		
+	}
+}
